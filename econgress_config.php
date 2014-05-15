3e0b550600035cd151c0d6d9acf539b75d64a14a
wagok <wlad@wlad.com.ua>
2014-05-15 21:08:31 +0400

    Update econgress_config.php

--- econgress_config.php
+++ econgress_config.php
@@ -4,6 +4,6 @@ define ( 'DB_PASSWORD', "" ); // database password
 define ( 'DB_HOSTNAME', "localhost" ); // database hostname
 define ( 'DB_DATABASE', "spr" ); // database name
 define ( 'FILE_NAME', 'econgress.php' ); // name of the basic file
-define ('SITE_PATH', 'http://svidokNew/');
+define ('SITE_PATH', 'http://');
 define ('DEBUG','');
 ?>
